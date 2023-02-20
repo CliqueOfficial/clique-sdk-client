8ab2f03554da13e7f0141918c83ce59134d7402a
Han Wu <xjohnwu@gmail.com>
2023-02-20 22:48:21 +0000

    Update statistic_flow.ts

--- src/statistic_flow.ts
+++ src/statistic_flow.ts
@@ -6,11 +6,11 @@ dotenv.config();
 const apiKey = String(process.env.API_KEY);
 const apiSecret = String(process.env.API_SECRET);
 const walletAddress = "";
-const twitterAccessToken = undefined;
-const discordAccessToken = undefined;
+const twitterAccessToken = 'dEJxNEFSejZ3UVpsU2pQWlpONG9uZ2hzY0hVdV9CSnJNWC12N2dPV3ZmYUZPOjE2NzY5MzI5NTYxMjA6MTowOmF0OjE';
+const discordAccessToken = 'QjsXtyDuZub4K4ns3sAOmfMj9aCZgW';
 const selectPipelines = undefined;
 const steamToken =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdGVhbUlkIjoiNzY1NjExOTgxMjM0NzA3NDEiLCJleHAiOjE2NzY2MTgyMDMwMjh9.lIt1-0xJkMOpL_A-hfR7zBjEXLcTcQc-0p3DGsUY8pI";
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdGVhbUlkIjoiNzY1NjExOTgwNDU3NzM4ODQiLCJleHAiOjE2NzY5NDA0MjY0Njh9.xABc70G3PBTt6Y9hfZjLLphy50-jlAAeurkNALl0-R8";
 
 (async () => {
       const client = new CliqueClient({
@@ -26,5 +26,5 @@ const steamToken =
             selectPipelines: selectPipelines,
             steamToken: steamToken,
       });
-      console.log(`statistic data ${JSON.stringify(data)}`);
+      console.log(`statistic data`, data);
 })();
